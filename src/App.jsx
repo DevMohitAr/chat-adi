0ffb349b8284ad7dfdf9beba87b58044d687e552
DevMohitAr <codingwithmohit.ar@gmail.com>
2023-12-13 12:34:36 +0530

    final commit for POC

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,31 @@ import { PiChatCircleDots } from "react-icons/pi";
 import { CiChat1 } from "react-icons/ci";
 import { IoSettingsOutline } from "react-icons/io5";
 import { useMutation, useQueryClient } from "react-query";
+import fetchWithError from "./components/FetchWithError";
 function App() {
   const [search, setSearch] = React.useState("");
   const [show, setShow] = React.useState(false);
   const [results, setResults] = React.useState([]);
-const resultsContainerRef = useRef(null);
-  const querClient = useQueryClient();
-    const scrollToLatestResult = () => {
-      if (resultsContainerRef.current) {
-        resultsContainerRef.current.scrollTop =
-          resultsContainerRef.current.scrollHeight;
-      }
-    };
-    React.useEffect(()=>{
-        scrollToLatestResult()
-    },[results])
+  const resultsContainerRef = useRef(null);
+  const url = "https://stage-api.adinvestor.com/api/v1/chat_api/";
+  const scrollToLatestResult = () => {
+    if (resultsContainerRef.current) {
+      resultsContainerRef.current.scrollTop =
+        resultsContainerRef.current.scrollHeight;
+    }
+  };
+  React.useEffect(() => {
+    scrollToLatestResult();
+  }, [results]);
   const searchMutation = useMutation(
     (search) =>
-      fetch("https://stage-api.adinvestor.com/api/v1/chat_api/", {
+      fetchWithError(url, {
         method: "POST",
         headers: {
           "content-type": "application/json",
         },
         body: JSON.stringify({ message: search }),
-      }).then((res) => res.json()),
+      }),
     {
       onSuccess: (data) => {
         setResults((prev) => [
@@ -46,6 +47,7 @@ const resultsContainerRef = useRef(null);
       onError: (error) => {
         console.log(error);
       },
+      enabled: !!search,
     }
   );
 
@@ -60,7 +62,7 @@ const resultsContainerRef = useRef(null);
   return (
     <section className="h-[90vh]">
       <main className="grid grid-cols-[1.5fr_5fr] h-full  px-10 gap-10 mt-10 mb-10">
-        <div className="bg-slate-100 p-3">
+        <div className="bg-slate-400 text-slate-100 p-3">
           <h1>Context</h1>
         </div>
         <div className="grid grid-cols-[1.25fr_3fr] h-full border-2 bg-slate-100 rounded-2xl p-1 shadow-md">
@@ -103,8 +105,8 @@ const resultsContainerRef = useRef(null);
             </div>
           </div>
           <div className="grid grid-rows-[auto_1fr_auto] bg-slate-100 rounded-md">
-            <div className="flex justify-between bg-slate-100 p-4 ">
-              <div>
+            <div className="flex justify-between bg-slate-200 p-4 ">
+              <div >
                 <h2>New Conversation</h2>
                 Suggested:
                 <small className="text-cyan-500 font-medium">
@@ -135,21 +137,42 @@ const resultsContainerRef = useRef(null);
                   {results?.map((item, i) => {
                     const { sql, data, query } = item;
                     return (
-                      <article className="mb-5" key={i}>
-                        <h2>Query--</h2>
-                        <p>{query}</p>
-                        <h2>Sql--</h2>
-                        <h3>{sql}</h3>
-                        <h2>Data--</h2>
-                        <p>{data}</p>
+                      <article
+                        className="mb-5 grid grid-cols-2 grid-rows-[auto_auto] gap-5"
+                        key={i}
+                      >
+                        <div className="col-start-2 bg-cyan-700 rounded-lg     text-slate-50">
+                          <p className="p-1 text-sm capitalize text-center">
+                            {query}
+                          </p>
+                        </div>
+                        <div className="row-start-2 bg-slate-400 flex flex-col gap-4 p-2 rounded-lg">
+                          <div>
+                            <h2 className="text-slate-100 font-bold mb-1">
+                              Sql--
+                            </h2>
+                            <h3 className="bg-slate-100 rounded-lg p-2">
+                              {sql}
+                            </h3>
+                          </div>
+                          <div className="flex flex-col gap-2">
+                            <h2 className="font-bold text-slate-100">Data--</h2>
+                            <div className="bg-slate-900 text-slate-50 p-2 rounded-lg">
+                              {data.map((item, i) => {
+                                return <p key={i}>{item}</p>;
+                              })}
+                            </div>
+                          </div>
+                        </div>
                       </article>
                     );
                   })}
                 </div>
-                <div>{searchMutation.isLoading && <p>Fetching...</p>}</div>
+                <div >{searchMutation.isLoading && <p >Fetching...</p>}</div>
+                <div>{searchMutation.isError &&<p>{searchMutation.error.message}</p>}</div>
               </div>
             </main>
-            <div className="p-2 bg-slate-100">
+            <div className="p-2 bg-slate-200">
               <Form onSubmit={handleSubmit} setSearch={setSearch} />
             </div>
           </div>
